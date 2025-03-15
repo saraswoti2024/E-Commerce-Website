d36c7377814234a5b5befcc045ba7670b04607e1
saraswoti2024 <saraswotikhadka2k2@gmail.com>
2025-03-15 22:23:47 +0545

    password change when password is not known using auth_veiws

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import *
+
 urlpatterns = [
      path('',home,name="home"),
     path('home/',home,name="home"),
@@ -11,5 +13,11 @@ urlpatterns = [
     path('login/',log_in,name="log_in"),
     path('register/',register,name="register"),
     path('logout/',log_out,name="log_out"),
+    #for forgot password when password is not known end
     path('change_password/',change_password,name="change_password"),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name = "auth/password_reset_complete.html"), name='password_reset_complete'),
+       #for forgot password when password is not known end
 ]
